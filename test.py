ff7dc5a39e34fae3060c183367916342e205da0c
Alex Han <ahan98@gmail.com>
2020-10-12 07:20:47 -0700

    cleanup

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ def k_fold_cross_val(data, k=None, shuffle_data=True):
     best_weights = None
     for i in range(k):
         train_data = groups[i]
-        test_data = [y for x in groups[:i] + groups[i+1:] for y in x]
+        test_data = [data for group in groups[:i] + groups[i+1:] for data in group]
         n_correct, n_total, w_hidden, w_out = test(train_data, test_data)
         acc = n_correct / n_total
         if acc > best_acc[0]:
@@ -34,7 +34,9 @@ def k_fold_cross_val(data, k=None, shuffle_data=True):
     return best_acc, best_weights
 
 
-def test(train_data, test_data, n_hidden=None, learn_rate=0.1, n_epochs=300):
+def test(train_data, test_data=None, n_hidden=None, learn_rate=0.1, n_epochs=300):
+    if test_data is None:
+        test_data = train_data
     w_hidden, w_out = backprop(train_data, n_hidden, learn_rate, n_epochs)
     preds = []
     targets = []
@@ -50,5 +52,6 @@ def test(train_data, test_data, n_hidden=None, learn_rate=0.1, n_epochs=300):
 if __name__ == "__main__":
     filename = parse_filename()
     data = build_data_from_arff(filename)
-    best_acc, best_weights = k_fold_cross_val(data, k=10)
-    print(best_acc)
+    test(data)
+    # best_acc, best_weights = k_fold_cross_val(data, k=2)
+    # print(best_acc)
