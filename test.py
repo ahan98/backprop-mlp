98e8d3bf403527989877d8ad09efc3fad372465c
Alex Han <ahan98@gmail.com>
2020-10-12 15:22:49 -0700

    cleanup accuracy logic

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from train import backprop, forward
-from utils import parse_filename, accuracy
+from utils import parse_filename
 from data import build_data_from_arff
 from random import shuffle
 
@@ -22,7 +22,9 @@ def k_fold_cross_val(data, k=None, shuffle_data=True):
 
     best_acc = (0, 0, 0)  # (% correct, n_correct, n_total)
     best_weights = None
+    cumulative_correct = cumulative_total = 0
     for i in range(k):
+        print("\nUsing group {} of {} as test data".format(i+1, k))
         train_data = groups[i]
         test_data = [data for group in groups[:i] + groups[i+1:] for data in group]
         n_correct, n_total, w_hidden, w_out = test(train_data, test_data)
@@ -30,28 +32,39 @@ def k_fold_cross_val(data, k=None, shuffle_data=True):
         if acc > best_acc[0]:
             best_acc = (acc, n_correct, n_total)
             best_weights = (w_hidden, w_out)
+        cumulative_correct += n_correct
+        cumulative_total += n_total
 
-    return best_acc, best_weights
+    avg_acc = 100 * cumulative_correct / cumulative_total
+    print("Average accuracy: {:.2f}%".format(avg_acc))
+    return avg_acc, best_acc, best_weights
 
 
-def test(train_data, test_data=None, n_hidden=None, learn_rate=0.1, n_epochs=300):
+def test(train_data, test_data=None, n_hidden=None, learn_rate=0.1,
+         n_epochs=300, verbose=True):
+
     if test_data is None:
         test_data = train_data
     w_hidden, w_out = backprop(train_data, n_hidden, learn_rate, n_epochs)
     preds = []
-    targets = []
+    n_correct = 0
+    n_total = len(test_data)
     for input, target in test_data:
         _, out = forward(input, w_hidden, w_out)
-        preds.append(np.argmax(out))
-        targets.append(target.index(1))
+        predicted_class_val = np.argmax(out)
+        preds.append(predicted_class_val)
+        n_correct += target[predicted_class_val]
 
-    n_correct, n_total = accuracy(preds, targets)
+    if verbose:
+        print("Percent classified correctly: {:.2f}% ({}:{})"
+              .format(100 * n_correct / n_total, n_correct, n_total - n_correct))
+
     return n_correct, n_total, w_hidden, w_out
 
 
 if __name__ == "__main__":
     filename = parse_filename()
     data = build_data_from_arff(filename)
-    test(data)
-    # best_acc, best_weights = k_fold_cross_val(data, k=2)
+    # test(data)
+    avg_acc, best_acc, best_weights = k_fold_cross_val(data, k=10)
     # print(best_acc)
--- utils.py
+++ utils.py
@@ -10,25 +10,3 @@ def parse_filename():
     parser.add_argument("data", type=str, help="path to .arff file")
     args = vars(parser.parse_args())
     return args["data"]
-
-
-def accuracy(list1, list2, verbose=True):
-    """
-    Computes the element-wise similarity between lists list1 and list2.
-
-    INPUTS:
-    - list1 [list]: list of arbitrary values
-    - list2 [list]: list of arbitrary values
-
-    RETURNS:
-    - n_correct [int]: total num. of elementwise matches
-    - n_total [int]: total num. elements compared
-    """
-    assert len(list1) == len(list2)
-    n_total = len(list1)
-    n_correct = sum([list1[i] == list2[i] for i in range(n_total)])
-
-    if verbose:
-        print("Percent classified correctly: {:.2f}% ({}/{})"
-              .format(100 * n_correct / n_total, n_correct, n_total))
-    return n_correct, n_total
